fd3d5796d792a6e819efcf56b2862184ba7eb081
Lucas Quevedo <lucas.qvdo@gmail.com>
2023-07-05 15:16:16 +0000

    Utilizado modelo OOP, add cadastro_bancario

--- busca_cnpj.py
+++ busca_cnpj.py
@@ -2,9 +2,63 @@ import requests, json
 from openpyxl import Workbook, load_workbook
 
 
-while True:
-    cnpj=input("Digite o CNPJ que será consultado:\n")
+class fornecedor:
+    def __init__(self, nome, fantasia, cnpj, logradouro, numero, bairro, uf, cep, telefone, email):
+        self.nome=nome
+        self.fantasia=fantasia
+        self.cnpj=cnpj
+        self.logradouro=logradouro
+        self.numero=numero
+        self.bairro=bairro
+        self.uf=uf
+        self.cep=cep
+        self.telefone=telefone
+        self.email=email
 
+def cadastro_principal():
+    
+    sheet['C4'].value=fornecedor.nome
+    sheet['C3'].value=fornecedor.fantasia
+    sheet['C5'].value=fornecedor.cnpj
+    sheet['C8'].value=fornecedor.logradouro
+    sheet['G8'].value=fornecedor.numero
+    sheet['B9'].value=fornecedor.bairro
+    sheet['D9'].value=fornecedor.uf
+    sheet['G9'].value=fornecedor.cep
+    sheet['B10'].value=fornecedor.telefone
+    sheet['B11'].value=fornecedor.email
+
+def cadastro_bancario():
+    confirma = "n"
+    while confirma != "s":
+
+        banco = input("Informe o codigo e nome do banco:\n")
+        agencia = input("Informe o numero da agencia:\n")
+        conta = input("Informe o número da conta:\n")
+        print("Confirmando: \n", "Banco:   ", banco, "\n" ,"Agencia: ", agencia, "\n","Conta:   ", conta)
+
+        confirma = input("Os dados estão corretos? s/n\n")
+    
+        sheet['B17'].value=banco
+        sheet['B18'].value=agencia
+        sheet['B19'].value=conta
+
+def salvar_planilha():
+
+    workbook.save(filename=data['nome']+".xlsx")
+    print("Planilha Salva")
+
+
+
+
+while True:
+    cnpj=0
+    workbook = load_workbook(filename="Cadastro.xlsx")
+    sheet = workbook.active
+
+
+    while len(str(cnpj)) != 14:
+        cnpj=input("Digite o CNPJ que será consultado:\n")
     url = "https://receitaws.com.br/v1/cnpj/" + cnpj
     params = {
         "param1": "value1",
@@ -19,22 +73,44 @@ while True:
 
     if response.status_code == 200:
         data = response.json()
-        print(data['nome'])
+        print("Empresa encontrada: ", data['nome'])
+        fornecedor=fornecedor(data['nome'], data['fantasia'], data['cnpj'], data['logradouro'], data['numero'], data['bairro'], data['uf'], data['cep'], data['telefone'], data['email'])
+        
+        respostacorreta= False
+        while respostacorreta == False:
 
-        workbook = load_workbook(filename="Cadastro.xlsx")
-        sheet = workbook.active
-        sheet['C7'].value=data['nome']
-        sheet['C8'].value=data['cnpj']
-        sheet['B11'].value=data['logradouro']
-        sheet['H11'].value=data['numero']
-        sheet['B12'].value=data['bairro']
-        sheet['D12'].value=data['uf']
-        sheet['H12'].value=data['cep']
-        sheet['B13'].value=data['telefone']
-        sheet['B14'].value=data['email']
+        
+            d_bancarios= input("Deseja adicionar os dados bancários ao cadastro? s/n \n")
+            if d_bancarios == "n" or d_bancarios == "N":
+                cadastro_principal()
+                salvar_planilha()
+                respostacorreta = True
+
+
+
+            elif d_bancarios == "s" or d_bancarios == "S":
+                cadastro_principal()
+                cadastro_bancario()
+        
+                salvar_planilha()
+                respostacorreta = True
+            
+            
+            else:
+                print("Digite 's' ou 'n':\n")
+                
+      
+
     
-        workbook.save(filename=data['nome']+".xlsx")
-        print("Planilha Salva")
+            
 
-    else:
-        print("Error: ", response.status_code)
+        
+        
+           
+
+        
+        
+    
+        
+
+    
